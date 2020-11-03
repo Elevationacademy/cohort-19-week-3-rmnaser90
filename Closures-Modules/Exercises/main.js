530108427c08baf8cd8f8be8aada0029381babbe
rmnaser90 <rmnaser90@gmail.com>
2020-11-03 16:05:42 +0200

    exercise 2 done

--- Closures-Modules/Exercises/main.js
+++ Closures-Modules/Exercises/main.js
@@ -1,30 +1,64 @@
-const stringFormatter= function (){
-let newStr
-const capitalizeFirst = function(str){
-newStr= str.slice(0,1).toUpperCase() + str.slice(1,str.length).toLowerCase()
-    console.log(newStr);
-    return newStr
+// Exercise 1
+
+// const stringFormatter= function (){
+// let newStr
+// const capitalizeFirst = function(str){
+// newStr= str.slice(0,1).toUpperCase() + str.slice(1,str.length).toLowerCase()
+//     console.log(newStr);
+//     return newStr
+// }
+
+// const skewerCase = function (str) {
+
+
+//     newStr = str.split(' ').join('-')
+//     console.log(newStr);
+//     return newStr
+// }
+
+
+
+// return {
+//     capitalizeFirst,
+//     skewerCase
+// }
+// }
+
+
+
+// const formatter = stringFormatter()
+
+// formatter.capitalizeFirst("dorothy") //should print Dorothy
+// formatter.skewerCase("blue box") //should print blue-box
+
+
+
+// Exercise 2
+const Bank = function () {
+    let balance= 500
+
+
+    const deposit = function(cash){
+balance+=cash
+
+    }
+
+    const showBalance = function () {
+        console.log(balance);
+
+    }
+ 
+    return {deposit,showBalance}
 }
 
-const skewerCase = function (str) {
-
-
-    newStr = str.split(' ').join('-')
-    console.log(newStr);
-    return newStr
-}
 
 
 
-return {
-    capitalizeFirst,
-    skewerCase
-}
-}
+const bank = Bank()
+bank.deposit(200)
+bank.deposit(250)
+bank.showBalance() //should print 950
 
-
-
-const formatter = stringFormatter()
-
-formatter.capitalizeFirst("dorothy") //should print Dorothy
-formatter.skewerCase("blue box") //should print blue-box
+const myBank = Bank()
+myBank.deposit(-200)
+myBank.showBalance()

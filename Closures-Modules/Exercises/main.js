e6b90703f4db8326cabbd9d81476f55f711753e6
rmnaser90 <rmnaser90@gmail.com>
2020-11-03 16:27:11 +0200

    exercise 3 done

--- Closures-Modules/Exercises/main.js
+++ Closures-Modules/Exercises/main.js
@@ -33,32 +33,56 @@
 
 
 
-// Exercise 2
-const Bank = function () {
-    let balance= 500
+// // Exercise 2
+// const Bank = function () {
+//     let balance= 500
 
 
-    const deposit = function(cash){
-balance+=cash
+//     const deposit = function(cash){
+// balance+=cash
 
+//     }
+
+//     const showBalance = function () {
+//         console.log(balance);
+
+//     }
+
+//     return {deposit,showBalance}
+// }
+
+
+
+
+// const bank = Bank()
+// bank.deposit(200)
+// bank.deposit(250)
+// bank.showBalance() //should print 950
+
+// const myBank = Bank()
+// myBank.deposit(-200)
+// myBank.showBalance()
+
+// Exercise 3
+
+const SongsManager = function () {
+    let songs = {}
+
+    const addSong = function (name, url) {
+        let URL = url.split('=')[1]
+        songs[name] = URL
+    }
+    const getSong = function (name) {
+
+        console.log("https://www.youtube.com/watch?v="+songs[name]);
     }
 
-    const showBalance = function () {
-        console.log(balance);
-
-    }
- 
-    return {deposit,showBalance}
+return {addSong,getSong}
 }
 
+const songsManager = SongsManager()
+songsManager.addSong("sax", "https://www.youtube.com/watch?v=3JZ4pnNtyxQ")
+songsManager.addSong("how long", "https://www.youtube.com/watch?v=CwfoyVa980U")
+songsManager.addSong("ain't me", "https://www.youtube.com/watch?v=D5drYkLiLI8")
 
-
-
-const bank = Bank()
-bank.deposit(200)
-bank.deposit(250)
-bank.showBalance() //should print 950
-
-const myBank = Bank()
-myBank.deposit(-200)
-myBank.showBalance()
+songsManager.getSong("sax") // should print https://www.youtube.com/watch?v=3JZ4pnNtyxQ
